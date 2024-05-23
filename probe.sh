dcd1e776c4cabd02495dab99ed17913636114acb
Dima Kogan <dima@secretsauce.net>
2024-05-23 16:39:00 -0700

    the topic is now required
    too much work was needed otherwise

--- probe.sh
+++ probe.sh
@@ -1,12 +1,12 @@
 #!/bin/zsh
 
 read -r -d '' usage <<EOF || true
-Usage: $0 [--plot] [--topic TOPIC] ELF_PUB ELF_SUB
+Usage: $0 [--plot] TOPIC ELF_PUB ELF_SUB
 
 SYNOPSIS
 
-  \$ $0 --plot libpublisher.so libsubscriber.so
-  [ a realtime plot of latencies for ALL messages pops up ]
+  \$ $0 --plot /topic libpublisher.so libsubscriber.so
+  [ a realtime plot of latencies for the given topic pops up ]
 
 DESCRIPTION
 
@@ -14,21 +14,16 @@ This is a wrapper around a bpftrace script for ros2 tracing of communication
 latencies. By default this runs bpftrace, and produces its output on stdout.
 With --plot, this output is plotted in realtime.
 
-If given, we only report the messages sent/received on the TOPIC. Otherwise we
-report on all the topics
-
 Arguments:
 
   --plot: if given, plot the latency in realtime
-
-  --topic TOPIC: if given, only look for communcation on TOPIC
 
 EOF
 
 # comma-separated list of long options. Trailing : means "has required
 # argument".
-longopts='plot,topic:'
-Ntrailing_options_required=2
+longopts='plot'
+Ntrailing_options_required=3
 
 # The parsing code to use getopt comes from
 #   /usr/share/doc/util-linux/examples/getopt-example.bash
@@ -49,12 +44,6 @@ while {true} {
 	        '--plot')
                         plot=1
                         shift
-                        continue
-                ;;
-
-                '--topic')
-                        topic=$2
-                        shift 2
                         continue
                 ;;
 
@@ -79,8 +68,9 @@ if (( $#* != Ntrailing_options_required )) {
 
 # Need the absolute path to work around this bug:
 #   https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=1063587
-PUB_option=$1; shift;
-SUB_option=$1; shift;
+topic=$1;        shift;
+PUB_option=$1;   shift;
+SUB_option=$1;   shift;
 
 PUB=${PUB_option:A};
 SUB=${SUB_option:A};
@@ -98,26 +88,14 @@ if ! [[ -f "$SUB" ]] {
        exit 1
 }
 
-if [[ -n "$topic" ]] {
-  event_match_condition="\$topic == \"$topic\""
-} else {
-  event_match_condition="1"
-  topic_legend=' topic'
-  topic_format=' %s'
-  topic_arg=',$topic'
-}
-
 # I print the legend here. bpftrace doesn't like my overly-long string, and I
 # don't want to ask for more bpf memory just to print out the legend
 cmd=(sudo zsh -c "echo '## Latency of received messages';
-                  echo '# t_pub_ns tid_pub cpu_pub t_latency_take_ns tid_take cpu_take t_latency_sub_ns tid_sub cpu_sub$topic_legend';
+                  echo '# t_pub_ns tid_pub cpu_pub t_latency_take_ns tid_take cpu_take t_latency_sub_ns tid_sub cpu_sub';
                   bpftrace -q <( <ros2-comm-trace.bt \
                                  | sed 's@{{PUB}}@$PUB@g;
-                                        s@{{SUB}}@$SUB@g;
-                                        s@{{EVENT_MATCH_CONDITION}}@$event_match_condition@g;
-                                        s@{{TOPIC_FORMAT}}@$topic_format@g;
-                                        s@{{TOPIC_ARG}}@$topic_arg@g;'
-                               )")
+                                        s@{{SUB}}@$SUB@g;'
+                               ) $topic")
 
 if ((plot)) {
     # The plotter is teed off. The data is always spit out to stdout
